7b4f6cabd52abb30b4cabde15ff85f8798c5cffa
yeoki-osu <yeoki@oregonstate.edu>
2024-02-26 22:07:50 -0800

    Created ac_api.py and test.py - Assignment 9 commit

---
+++ ac_api.py
@@ -0,0 +1,42 @@
+# python -m pip install --upgrade pip --user
+# pip install Flask requests
+# https://api.nookipedia.com/doc#/paths/~1villagers/get
+
+import requests
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+BASE_URL = "https://api.nookipedia.com/villagers"
+API_KEY = "26b6d7c2-a054-4308-bce3-4f8c956cc675"
+
+def query_characters(species="", gender="", personality="", name=""):
+    params = {
+        "api_key": API_KEY,
+        "species": species,
+        "gender": gender,
+        "personality": personality,
+        "name": name
+    }
+    response = requests.get(BASE_URL, params=params)
+    if response.status_code == 200:
+        characters = response.json()
+        if gender:
+            characters = [character for character in characters if character.get('gender', '').lower() == gender.lower()]
+        return characters
+    else:
+        return []
+
+@app.route('/query', methods=['POST'])
+def query():
+    request_data = request.json
+    species = request_data.get('species', '')
+    gender = request_data.get('gender', '')
+    personality = request_data.get('personality', '')
+    name = request_data.get('name', '')
+
+    characters = query_characters(species, gender, personality, name)
+    return jsonify(characters)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ test.py
@@ -0,0 +1,32 @@
+# curl -X POST -H "Content-Type: application/json" -d '{"species": "", "gender": "", "personality": "", "name": ""}' http://127.0.0.1:5000/query
+
+import requests
+import json
+
+def query_characters(json_data):
+    url = "http://127.0.0.1:5000/query"
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(url, headers=headers, data=json.dumps(json_data))
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return []
+
+def main():
+    species = "wolf"
+    gender = "female"
+    personality = "peppy"
+    name = ""
+
+    json_data = {
+        "species": species,
+        "gender": gender,
+        "personality": personality,
+        "name": name
+    }
+
+    characters = query_characters(json_data)
+    print(characters)
+
+if __name__ == "__main__":
+    main()
